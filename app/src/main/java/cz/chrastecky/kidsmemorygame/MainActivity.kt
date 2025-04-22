484a38cf10a32f0aabfb227b4ce3e1d590329964
Dominik Chrastecky <dominik@chrastecky.cz>
2025-04-22 21:45:09 +0200

    Make theme chooser work

--- app/src/main/java/cz/chrastecky/kidsmemorygame/MainActivity.kt
+++ app/src/main/java/cz/chrastecky/kidsmemorygame/MainActivity.kt
@@ -9,12 +9,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +47,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,6 +59,8 @@ import cz.chrastecky.kidsmemorygame.theme_provider.ThemeInfo
 import cz.chrastecky.kidsmemorygame.theme_provider.ThemeProvider
 import cz.chrastecky.kidsmemorygame.theme_provider.provider.LocalAssetsThemeProvider
 import cz.chrastecky.kidsmemorygame.ui.theme.BackgroundColor
+import cz.chrastecky.kidsmemorygame.ui.theme.CardBackground
+import cz.chrastecky.kidsmemorygame.ui.theme.CardBorder
 import cz.chrastecky.kidsmemorygame.ui.theme.KidsMemoryGameTheme
 import kotlinx.coroutines.delay
 
@@ -141,13 +168,69 @@ fun ThemePickerScreen(
     onThemeSelected: (ThemeInfo) -> Unit
 ) {
     Box(
-        modifier = Modifier.fillMaxSize().background(BackgroundColor),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundColor)
     ) {
         Image(
-            painter = painterResource(id = R.drawable.main_menu_bg),
+            painter = painterResource(id = R.drawable.theme_picker_bg),
+            contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier.matchParentSize(),
-            contentDescription = null,
         )
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(4),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 180.dp, end = 180.dp)
+                    .fillMaxHeight()
+                    .wrapContentHeight()
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                items(themes) { theme ->
+                    ThemeCard(theme = theme, onClick = { onThemeSelected(theme) })
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ThemeCard(theme: ThemeInfo, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable{ onClick() }
+        ,
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .border(2.dp, CardBorder, RoundedCornerShape(16.dp))
+                .clickable { onClick() }
+                .background(BackgroundColor)
+                .width(120.dp)
+                .height(120.dp)
+            ,
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                bitmap = theme.icon.asImageBitmap(),
+                contentDescription = theme.name,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(CardBackground)
+                ,
+                contentScale = ContentScale.Fit
+            )
+        }
     }
 }
--- app/src/main/java/cz/chrastecky/kidsmemorygame/ui/theme/Color.kt
+++ app/src/main/java/cz/chrastecky/kidsmemorygame/ui/theme/Color.kt
@@ -11,3 +11,5 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val BackgroundColor = Color(0xFFFDB842)
+val CardBackground = Color(0xFFFFE5A5)
+val CardBorder = Color(0xFFAF6E3B)
