39ad53cd98476ef5131a592c8f71c7df52354cb4
Dominik Chrastecky <dominik@chrastecky.cz>
2025-04-30 21:18:05 +0200

    Chore: Optimize imports

--- app/src/main/java/cz/chrastecky/kidsmemorygame/ui/component/GameCard.kt
+++ app/src/main/java/cz/chrastecky/kidsmemorygame/ui/component/GameCard.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
