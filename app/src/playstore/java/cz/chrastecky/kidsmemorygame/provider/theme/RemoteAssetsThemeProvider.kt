53ef702b359461b3d1d25d7ec9cdea2559e8b47d
Rikudou_Sage <dominik@chrastecky.cz>
2025-05-03 21:12:47 +0200

    Chore: Install only needed dependencies for each flavour (#22)

---
+++ app/src/playstore/java/cz/chrastecky/kidsmemorygame/provider/theme/RemoteAssetsThemeProvider.kt
@@ -0,0 +1,25 @@
+package cz.chrastecky.kidsmemorygame.provider.theme
+
+import android.content.Context
+import cz.chrastecky.kidsmemorygame.dto.ThemeDetail
+import cz.chrastecky.kidsmemorygame.dto.ThemeInfo
+import cz.chrastecky.kidsmemorygame.provider.ThemeProvider
+
+// dummy class to make stuff compile
+class RemoteAssetsThemeProvider(context: Context) : ThemeProvider {
+    override suspend fun listAvailableThemes(): List<ThemeInfo> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun isThemeDownloaded(id: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getThemeDetail(id: String): ThemeDetail {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun download(id: String, onProgress: (Float) -> Unit) {
+        TODO("Not yet implemented")
+    }
+}
