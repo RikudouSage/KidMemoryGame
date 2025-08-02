75f15cb5f64cb7a17181233c5c5b0eba8d110b97
Dominik Chrastecky <dominik@chrastecky.cz>
2025-08-02 21:34:48 +0200

    Increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "cz.chrastecky.kidsmemorygame"
         minSdk = 24
         targetSdk = 35
-        versionCode = 26
-        versionName = "1.7.5"
+        versionCode = 27
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
